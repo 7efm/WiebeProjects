ba9b59c51f4498efd95633a5e1674fb57e5fc787
7efm <38299622+7efm@users.noreply.github.com>
2019-09-01 20:03:13 -0700

    9.1.19
    - Added rocket that follows path
    - Added comet that follows path
    - Added background detail

--- Animation Project/Animation1/src/Animate.java
+++ Animation Project/Animation1/src/Animate.java
@@ -5,26 +5,29 @@ final public class Animate {
 
     JFrame frame;
     DrawPanel drawPanel;
+    private int sleeper = 0;
+    private int opacity = 255;
     private int xSizePanel = 1500;
     private int ySizePanel = 800;
     private Color BlueL = new Color(54,155,172);
     private Color whiteTrans = new Color(255, 255, 255, 100);
-    private int oneX = 200; // Starting X coordinate
-    private int oneY = 200; // Starting Y coordinate
-    
+    private Color BrownL = new Color(242, 233, 228);
+    private Color orangeRedTrans = new Color(250, 121, 33, opacity);
+    private Color green2 = new Color(155, 197, 61);
+    private int fire = 0;
     private int cometX = 0;//private int cometX = -1000;
     private int cometY = 0;//private int cometY = -300;
-    private int rocketX = 100;
-    private int rocketY = 500;
-    private int dotW = 50;
-    private int dotH = 50;
-
-    private int angle = 20;
+    private int dotW = 0;
+        		
+    
     boolean up = false;
     boolean down = true;
     boolean left = false;
     boolean right = true;
-    int[] xArr = new int[]{100, 100, 0};  int[] yArr = new int[]{50, 0, 25};
+    
+    int[] xCont = new int[] {-20, 50, 30, 300, 350, 300, 200}; int[] yCont = new int[] {320, 400, 600, 800, 600, 420, 340};
+    int[] xArr = new int[]{100, 115, 117, 102};  int[] yArr = new int[]{400, 395, 400, 405};//rocket
+    int[] xArrR = new int[] {101, 102, 100, 98}; int[] yArrR = new int[] {401, 403, 405, 401};//rocket thrusts
     public static void main(String[] args) {
         new Animate().go();
     }
@@ -55,21 +58,29 @@ final public class Animate {
             //Inside Color
             g.setColor(Color.BLACK);
             g.fillRect(0,0,this.getWidth(), this.getHeight());
+            //Comet
             g.setColor(whiteTrans);
             g.fillArc(cometX, cometY, 100, 100, 325, 10 );
             g.setColor(new Color(255, 255, 255, 200));
             g.fillOval(cometX+90, cometY+71, 13, 13);
                         
-            g.setColor(Color.WHITE);
-            g.fillOval(oneX, oneY, dotW, dotH);
+            
             
             //Earth
             g.setColor(BlueL);
             g.fillOval(-350, frame.getHeight()-500, 800, 800);
+            g.setColor(green2);
+            g.fillPolygon(xCont, yCont, 7);
+            g.fillOval(20, 400, 20, 20);
+            g.setColor(BlueL);
+            g.fillOval(20, 600, 70, 50);
             //Moon 
             g.setColor(Color.LIGHT_GRAY);
             g.fillOval(frame.getWidth()-200, frame.getHeight()-500, 100, 100);
-            
+            g.setColor(new Color(0,0,0, 100));
+            g.fillOval(1360, 350, 20, 17);
+            g.fillOval(1320, 310, 30, 27);
+            g.fillOval(1315, 360, 10, 17);
             //Stars
             g.setColor(Color.WHITE);
         	g.fillRect(1200,123, 1, 1);
@@ -79,12 +90,19 @@ final public class Animate {
             g.fillRect(400,130, 2, 2);
             g.fillRect(850,550, 1, 1);
             g.fillRect(400,290, 2, 3);
-            //Rocket
-            g.setColor(Color.pink);
-            g.fillRoundRect(rocketX, rocketY, 20, 50, 10, 10);
-         // Comet
-            g.setColor(Color.WHITE);
-            g.fillPolygon(xArr, yArr, 3);
+            // Rocket
+            g.setColor(Color.GRAY);
+            g.fillOval(xArr[1]-3, yArr[1]-1, 8, 6);
+            g.setColor(orangeRedTrans);
+            g.fillArc(xArr[0]-25, yArr[0]-5, 25, 25, 10, fire);
+            g.setColor(Color.GRAY); 
+            g.fillPolygon(xArrR, yArrR, 4);
+            g.setColor(BrownL);
+            g.fillPolygon(xArr, yArr,4);
+            //Explosion
+            g.setColor(Color.RED);
+            g.fillArc(1312, 300, 25, 25, 10, dotW);
+            
             
             
             
@@ -96,69 +114,68 @@ final public class Animate {
     private void moveDot() {
         while(true){
             checkBounds();
-            moveLoc();
             startComet();
-            startRocket();
             try{
                 Thread.sleep(10);
             } catch (Exception exc){}
+            startRocket();
+
             frame.repaint();
         }
     }
     private void startComet() {
     	cometX+=2;
     	cometY+= 1;
-    	angle++;
     }
     private void startRocket(){
+    	if(sleeper > 100) {
     	for(int i = 0; i < xArr.length; i++) {
-    		xArr[i]+= 4;
-    		yArr[i]+= 1;
+    		xArr[i] += 1;
+    		xArrR[i] += 1;
+    		if(dotW > 0) {
+    			dotW -= 20;
+        		
+    		}
+    		
+    		int x = xArr[i]; 
+    	    double yVal = .00065*(x*x) -x +500;
+    	    yArr[0] = (int)yVal;	yArrR[0] = (int)yVal +1;
+    	    yArr[1] = (int)yVal -5; yArrR[1] = (int)yVal+3;
+    	    yArr[2] = (int)yVal;	yArrR[2] = (int)yVal+6;
+    	    yArr[3] = (int)yVal+5;	yArrR[3] = (int)yVal+1;
+    	    if(xArr[0] > frame.getWidth() -800 && fire > 0) {
+    	    	fire --;
+    	    }else if(xArr[0] < frame.getWidth() - 800) {
+    	    	fire = 23;
+    	    }
     	}
+    	}
+    	sleeper++;
+
     }
-    private void moveLoc(){
-    	if(up){
-    		oneY--;
-                      
-        }
-        if(down){
-            oneY++;
-        }
-        if(left){
-            oneX--;
-        }
-        if(right){
-            oneX++;
-        }	
-    }
+    
     //
     private void checkBounds(){
-    	if(oneX+dotW  >= frame.getWidth()){
-            right = false;
-            left = true;
-        }
-        if(oneX <= 0){
-            right = true;
-            left = false;
-        }
-        if(oneY+dotH >= frame.getHeight()){
-            up = true;
-            down = false;
-        }
-        if(oneY <= 0){
-            up = false;
-            down = true;
-        }
+    	
+        
         if( (cometX > frame.getWidth() +500) && (cometY >frame.getHeight()+ 500) ) {
         	cometX = -1000;
         	cometY = -300;
         }
-        for(int i = 0; i <xArr.length; i++) {
-        	if(xArr[i] > frame.getWidth()+ 500) {
-        		xArr[0] -= frame.getWidth() + 1000;
-        		xArr[1] -= frame.getWidth() + 1000;
-        		xArr[2] -= frame.getWidth() + 1000;
+        
+        if(xArr[1] > frame.getWidth() -175 ){
+        	dotW += 360;
+    		
+            xArr[0] = 100;
+        	yArr[0] = 400;
+        	xArr[1] = 115;
+        	yArr[1] = 395;
+        	xArr[2] = 117;
+        	yArr[2] = 400;
+        	xArr[3] = 102;
+        	yArr[3] = 405;
+        	xArrR = new int[] {101, 102, 100, 98}; yArrR = new int[] {401, 403, 405, 401};
         	}
         }
     }
-}
+    
